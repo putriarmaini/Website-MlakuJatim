101e6bf33a668d388109beb7c8e4ff7ce843f73a
Arull44 <63081155+Arull44@users.noreply.github.com>
2021-01-14 23:10:37 +0700

    done kak
    inshaallah fixed

--- login.js
+++ login.js
@@ -1,23 +1,29 @@
-function login()
-{
-    let username = document.querySelector("#username");
-    let password = document.querySelector("#password");
-
-    if(username.value == "")
-        {
-            alert("Masukkan email atau mobile number");
-            username.focus();
-            window.location = "login 1.html";
-            return false;
-        }
-    else if(password.value == "")
-        {
-            alert("Masukkan password");
-            password.focus();
-            window.location = "login 1.html";
-            return false;
-        }
-    else{
-            alert("Data berhasil disimpan");
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    
+    if(username == "ginogilang@gmail.com" && password == "12345") {
+        window.location = "index.html"
+    }
+    else if(username == "085123456789" && password == "12345") {
+        window.location = "index.html"
+    }
+    else if(username == "") {
+        alert("Masukkan email atau mobile number");
+        username.focus();
+    }
+    else if(password == "") {
+        alert("Masukkan password");
+        password.focus();
     }
 }
+
+$(document).ready(function(){		
+    $('.form-checkbox').click(function(){
+        if($(this).is(':checked')){
+            $('.form-password').attr('type','text');
+        }else{
+            $('.form-password').attr('type','password');
+        }
+    });
+});
